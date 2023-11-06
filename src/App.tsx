7b1a13211fdeb505640ee87d5397f87f3d04c70b
n3t3r4 <arthurleffa@gmail.com>
2023-11-06 12:02:31 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Start } from "./components/Start";
+import { Desktop } from "./components/Desktop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="bg-[#008080]">
+        <div className="min-h-screen flex flex-col justify-between">
+          <Desktop />
+          <Start />
+        </div>
+      </section>
+    </>
   );
 }
 
