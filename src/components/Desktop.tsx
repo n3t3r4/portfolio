ce8d2fdc3f7d70289be277c5746b784a32bd7f0a
n3t3r4 <arthurleffa@gmail.com>
2023-11-09 21:03:01 -0300

    selectable

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -7,25 +7,25 @@ export function Desktop() {
   return (
     <>
       <div className="z-0 flex flex-col items-start justify-center pt-2 text-white">
-        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px] max-w-[80px] ">
+        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px] max-w-[80px] selectable">
           <div>
             <img src={ProjetosIcon} className="max-w-[90px] my-[-20px]" />
           </div>
           <div>Projetos</div>
         </div>
-        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px]">
+        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px] selectable">
           <div>
             <img src={CurriculoIcon} className="max-w-[90px] my-[-20px]" />
           </div>
           <div>Currículo</div>
         </div>
-        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px]">
+        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px] selectable">
           <div>
             <img src={ContatoIcon} className="max-w-[90px] my-[-20px]" />
           </div>
           <div>Contato</div>
         </div>
-        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px]">
+        <div className="flex flex-col items-center m-3 cursor-pointer min-w-[80px] selectable">
           <div>
             <img src={InfoIcon} className="max-w-[90px] my-[-20px]" />
           </div>
